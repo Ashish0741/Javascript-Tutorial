c5912dc3b91b722de2309c9c500c64659afb36b7
Ashish Khedekar <60035837+Ashish0741@users.noreply.github.com>
2023-11-24 19:27:17 +0530

    Added Reduce Method and Chaining and OOPS
    Completed reduce method and chaining from Functional Programming further started with object oriented programming in which I have experimented this keyword behaviour for node.js environment and browser.

--- script.js
+++ script.js
@@ -159,5 +159,384 @@
 
 // ************************************************************************************************
 
+// find and findIndex methods :
+
+// find will returns the first element of an array that satisfies the condition
+
+// const transaction = [1000,3000,4000,2000,-898,3800,-4500]
+
+// let firstWithdrawl = transaction.find(function(amount){
+//     return amount < 0
+// })
+
+// console.log('First Withdrawl : ',firstWithdrawl)
+
+
+// // findIndex will returns the first element index from an array that satisfies the condition 
+
+// let firstWithdrawlIndex = transaction.findIndex(function(amount){
+//     return amount < 0
+// })
+
+// console.log('Index of First Withdrawl : ',firstWithdrawlIndex)
+
+// ===============================================================================================
+
+// some and every :
+
+// some will return true or false based on condition 
+// it will return true even if one satisfies the condition
+
+
+// const transaction = [-1000,-3000,-4000,-2000,898,-3800,-4500]
+
+// let resultSome = transaction.some(function(amount){
+//     return amount > 0
+// })
+
+// console.log('The result of some : ',resultSome)
+
+
+// // every will return true or false based on condition 
+// // it will return true if all satisfies the condition
+
+// let resultEvery = transaction.every(function(amount){
+//     return amount < 0
+// })
+
+// console.log('The result of every : ',resultEvery)
+
+// ************************************************************************************************
+
+// Filter method :
+
+// filter method will return new array it will return all those elements that satisfies the specific condition
+
+// let arr = [1,3,5,6,8,88,35,454,6457]
+
+// let evenArr = arr.filter(function(num){
+//     return num % 2 == 0
+// })
+
+// console.log('Even numbers : ',evenArr)
+
+
+// // find all transactions which are positive
+
+// const transaction = [1000,3000,4000,-2000,-898,3800,-4500]
+
+// let depositAmount = transaction.filter(function(amount){
+//     return amount > 0
+// })
+
+// console.log('Credited Amount : ',depositAmount)
+
+// ==============================================================================
+
+// Reduce method :
+// It will return one result value after performing some operations inside function
+// function which is passed as parameter takes two value (accumulator and value)
+
+// problem : you need to sum every even element of array
+
+// imperative way :
+
+// let arr = [1,2,3,5,10]
+
+// let sumOfEven = 0
+
+// for (let i = 0; i < arr.length; i++) {
+//     sumOfEven += (arr[i] % 2 == 0 ? arr[i] : 0)
+// }
+
+// console.log('Imperative way of finding the even number sum of array : ',sumOfEven)
+
+
+// declarative way using reduce method:
+
+// let arr = [1,2,3,5,10]
+
+// let sumOfEven = arr.reduce((accumulator, value)=>{
+//     return (value % 2 == 0 ? accumulator + value : accumulator + 0)
+// }, 0)
+
+// console.log('Declarative way of finding the even number sum of array : ',sumOfEven)
+
+// ************************************************************************************************
+
+// Chaining :
+// chaining will help to process multiple higher order functions in same line one after another.
+
+// problem 1 : you need to filter out all males and return ages of all males
+
+// let persons = [
+//     {name : 'A',age : 23,gender : 'M'},
+//     {name : 'B',age : 24,gender : 'F'},
+//     {name : 'C',age : 27,gender : 'F'},
+//     {name : 'D',age : 29,gender : 'M'},
+//     {name : 'E',age : 34,gender : 'M'},
+//     {name : 'F',age : 36,gender : 'F'},
+//     {name : 'G',age : 21,gender : 'M'},
+//     {name : 'H',age : 19,gender : 'F'},
+// ]
+
+// will solve without using chaining
+
+// let males = persons.filter(function(male){
+//     return male.gender == 'M'
+// })
+
+// let malesAges = males.map((male)=>{
+//     return male.age
+// })
+
+// console.log('Ages of all males in array without using chaining : ',malesAges)
+
+
+// using chaining
+
+// let maleAges = persons.filter(function(males){
+//     return males.gender == 'M'
+// }).map((male)=>{
+//     return male.age
+// })
+
+// console.log('Ages of all males in array using chaining : ',maleAges)
+
+// Problem 2 : 
+// from transaction array filter out positive amount and calculate the total amount 
+
+// let transaction = [1000,3000,4000,2000,-898,3800,-4500]
+
+// let totalAmount = transaction.filter((amount)=>{
+//     return amount > 0
+// }).reduce((acc,value)=>{
+//     return acc +  value
+// },0)
+
+// console.log('Total amount of all positive element : ',totalAmount)
+
+
+// ************************************************************************************************
+
+// Object Oriented Programming :
+
+// It is computer programming model that organizes software design around data or objects rather than function or logic.
+// An object can be defined as data field that has unique attributes and behaviour.
+
+// 4 pillars of oops :
+
+// 1. Encapsulation
+// 2. Abstraction
+// 3. Polymorphism
+// 4. Inheritance
+
+// IMP : javascript is not actually an object oriented language but we can write object oriented code by using 
+// something called as prototype object
+
+// ================================================================================================================
+
+// this keyword :
+// this keyword refers to an object
+
+// ========================================================================================
+
+// this keyword in node.js runtime env under non-strict mode :
+
+// -----------------------------------------------------
+// 1. on console :
+
+// console.log(this) // returns empty object
+
+// -----------------------------------------------------
+
+// 2. inside function :
+
+// function displayThis() {
+//     console.log(this) // returns global object
+// }
+
+// -----------------------------------------------------
+
+// 3. inside object --> function :
+
+// let myObj = {
+//     name : 'Ashish',
+//     age : 23,
+//     myFun : function(){
+//         console.log(this) // returns object itself
+//     }
+// }
+
+// myObj.myFun()
+
+// -----------------------------------------------------
+
+// 4. inside object --> function --> function :
+
+
+// let myObj = {
+//     name : 'Ashish',
+//     age : 23,
+//     myFun : function(){
+//         function myFun1() {
+//             console.log(this) // returns global object
+//         }
+//         myFun1()
+//     }
+// }
+
+// myObj.myFun()
+
+// ===========================================================================================
+
+// this keyword in node.js runtime env under strict mode :
+// 'use strict'  // need to mention this string on top of code for using strict mode
+
+// -----------------------------------------------------
+// 1. on console :
+
+// console.log(this) // returns empty object
+
+// -----------------------------------------------------
+
+// 2. inside function :
+
+// function displayThis() {
+//     console.log(this) // returns undefined
+// }
+
+// -----------------------------------------------------
+
+// 3. inside object --> function :
+
+// let myObj = {
+//     name : 'Ashish',
+//     age : 23,
+//     myFun : function(){
+//         console.log(this) // returns object itself
+//     }
+// }
+
+// myObj.myFun()
+
+// -----------------------------------------------------
+
+// 4. inside object --> function --> function :
+
+
+// let myObj = {
+//     name : 'Ashish',
+//     age : 23,
+//     myFun : function(){
+//         function myFun1() {
+//             console.log(this) // returns undefined
+//         }
+//         myFun1()
+//     }
+// }
+
+// myObj.myFun()
+
+// ===========================================================================================
+
+// this keyword in browser under non-strict mode :
+
+// -----------------------------------------------------
+// 1. on console :
+
+// console.log(this) // returns window object
+
+// -----------------------------------------------------
+
+// 2. inside function :
+
+// function displayThis() {
+//     console.log(this) // returns window object
+// }
+
+// -----------------------------------------------------
+
+// 3. inside object --> function :
+
+// let myObj = {
+//     name : 'Ashish',
+//     age : 23,
+//     myFun : function(){
+//         console.log(this) // returns object itself
+//     }
+// }
+
+// myObj.myFun()
+
+// -----------------------------------------------------
+
+// 4. inside object --> function --> function :
+
+
+// let myObj = {
+//     name : 'Ashish',
+//     age : 23,
+//     myFun : function(){
+//         function myFun1() {
+//             console.log(this) // returns window object
+//         }
+//         myFun1()
+//     }
+// }
+
+// myObj.myFun()
+
+// ===========================================================================================
+
+// this keyword in browser under strict mode :
+// 'use strict'  // need to mention this string on top of code for using strict mode
+
+// -----------------------------------------------------
+// 1. on console :
+
+// console.log(this) // returns window object
+
+// -----------------------------------------------------
+
+// 2. inside function :
+
+// function displayThis() {
+//     console.log(this) // returns undefined
+// }
+
+// -----------------------------------------------------
+
+// 3. inside object --> function :
+
+// let myObj = {
+//     name : 'Ashish',
+//     age : 23,
+//     myFun : function(){
+//         console.log(this) // returns object itself
+//     }
+// }
+
+// myObj.myFun()
+
+// -----------------------------------------------------
+
+// 4. inside object --> function --> function :
+
+
+// let myObj = {
+//     name : 'Ashish',
+//     age : 23,
+//     myFun : function(){
+//         function myFun1() {
+//             console.log(this) // returns undefined
+//         }
+//         myFun1()
+//     }
+// }
+
+// myObj.myFun()
+
+// ************************************************************************************************
 
 
